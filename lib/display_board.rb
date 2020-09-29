063cd46cafccbc2ffd8d1f9997808e385dcd8821
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-29 03:09:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,11 +4,11 @@
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
 def display_board(array)
-  puts = " #{array[0]} | #{array[1]} | #{array[2]} "
-  puts = "-----------"
-  puts = " #{array[3]} | #{array[4]} | #{array[5]} "
-  puts = "-----------"
-  puts = " #{array[6]} | #{array[7]} | #{array[8]} "
+  " #{array[0]} | #{array[1]} | #{array[2]} "
+  "-----------"
+  " #{array[3]} | #{array[4]} | #{array[5]} "
+  "-----------"
+  " #{array[6]} | #{array[7]} | #{array[8]} "
 end
 
 display_board(board)
